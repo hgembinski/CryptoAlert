56ffb5d5b781d4645043fd5949f7e3e344baa922
Haiden <hgembinski@gmail.com>
2021-07-12 03:50:22 -0400

    ca_gui init

---
+++ ca_gui.py
@@ -0,0 +1,20 @@
+#Haiden Gembinski
+#GUI functionality for CryptoAlert
+
+import tkinter
+from tkinter import *
+
+#Settings screen display
+def settings_screen(root):
+
+    return
+
+#History screen display
+def history_screen(root):
+
+    return
+
+#Error screen display
+def error_screen(root):
+
+    return
