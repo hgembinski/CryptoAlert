d758d1515ef70e86412e72751df40d3dd0c31b5b
Haiden <hgembinski@gmail.com>
2021-07-13 03:03:48 -0400

    to/than text switch in settings

--- ca_gui.py
+++ ca_gui.py
@@ -7,6 +7,7 @@ from tkinter import font
 from tkinter import ttk
 from ttkwidgets.autocomplete import AutocompleteCombobox
 
+
 #Settings screen display
 def settings_screen(root):
     root.withdraw() #hide root
@@ -46,18 +47,31 @@ def settings_screen(root):
     changes_text = Label(settings, text = "Changes by ", bg = "azure", fg = "#000F46",
                 font = (None, 22)).place(x = 250, y = 225)
     changes_percent = Label(settings, text = "%", bg = "azure", fg = "#000F46",
-                font = (None, 22)).place(x = 500, y = 225)
-    changes_number = Entry(settings, font = (None, 20), width = 4, relief = "sunken").place(x = 420, y = 227)
+                font = (None, 22)).place(x = 490, y = 225)
+    changes_number = Entry(settings, font = (None, 20), width = 4, relief = "sunken").place(x = 425, y = 227)
     
     price_is = Canvas(settings, bg = "light grey", width = 25, height = 25).place(x = 200, y = 296) #'is' toggle
     is_text = Label(settings, text = "Is ", bg = "azure", fg = "#000F46",
                 font = (None, 22)).place(x = 250, y = 292)
     is_choices = ["=", "<", ">"]
     is_choice_dropdown = AutocompleteCombobox(settings, width = 2, font = (None, 20), justify = "center",
-                completevalues = is_choices, state = "readonly" ).place(x = 300, y = 294)
+                completevalues = is_choices, state = "readonly")
+    is_choice_dropdown.place(x = 300, y = 294)
+    to_than_text = Label(settings, text = "to", bg = "azure", fg = "#000F46",
+            justify = "center", font = (None, 22))
+    to_than_text.place(x = 375, y = 292)
+
+    #changes text to to/than depending on the sign selected in dropdown
+    def to_than_change(eveny):
+        if is_choice_dropdown.get() == "=":
+            to_than_text.config(text = "  to")
+        else:
+            to_than_text.config(text = "than")
+
+    is_choice_dropdown.bind("<<ComboboxSelected>>", to_than_change)
     is_sign = Label(settings, text = "$", bg = "azure", fg = "#000F46",
-                font = (None, 22)).place(x = 375, y = 292)
-    is_number = Entry(settings, font = (None, 20), width = 8, relief = "sunken").place(x = 400, y = 294)
+                font = (None, 22)).place(x = 450, y = 292)
+    is_number = Entry(settings, font = (None, 20), width = 8, relief = "sunken").place(x = 475, y = 294)
     
     alert_config_text = Label(settings, text = "Alert me by...", bg = "azure", fg = "#000F46",
                 font = (None, 30, "italic")).place(x = 150, y = 365)
@@ -81,7 +95,7 @@ def settings_screen(root):
                 text = "Cancel", relief = "raised", width = 10, font = (None, 30, "bold"), command = lambda: settings_cancel(root, settings))
     cancel.place(x = 200, y = 650, anchor = "center")
 
-    return
+
 
 #Settings confirm button
 def settings_confirm(root, settings):
