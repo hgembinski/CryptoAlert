7f6452df37081c471c313539b17640062280f476
Haiden <hgembinski@gmail.com>
2021-07-13 05:46:05 -0400

    start work on settings

--- ca_gui.py
+++ ca_gui.py
@@ -9,7 +9,7 @@ from ttkwidgets.autocomplete import AutocompleteCombobox
 
 
 #Settings screen display
-def settings_screen(root, coin, price, alert_type, alert_number, is_sound, is_email, email):
+def settings_screen(root, coin, initial_price, alert_type, alert_number, is_sound, is_email, email):
     root.withdraw() #hide root
     settings = tkinter.Toplevel(root)
     settings.title("CryptoAlert Settings")
@@ -56,7 +56,7 @@ def settings_screen(root, coin, price, alert_type, alert_number, is_sound, is_em
     is_choice_dropdown = AutocompleteCombobox(settings, width = 2, font = (None, 20), justify = "center",
                 completevalues = is_choices, state = "readonly")
     is_choice_dropdown.place(x = 300, y = 294)
-    to_than_text = Label(settings, text = "to", bg = "azure", fg = "#000F46",
+    to_than_text = Label(settings, text = "  to", bg = "azure", fg = "#000F46",
             justify = "center", font = (None, 22))
     to_than_text.place(x = 375, y = 292)
 
@@ -70,7 +70,8 @@ def settings_screen(root, coin, price, alert_type, alert_number, is_sound, is_em
     is_choice_dropdown.bind("<<ComboboxSelected>>", to_than_change)
     is_sign = Label(settings, text = "$", bg = "azure", fg = "#000F46",
                 font = (None, 22)).place(x = 450, y = 292)
-    is_number = Entry(settings, font = (None, 20), width = 8, relief = "sunken").place(x = 475, y = 294)
+    is_number = Entry(settings, font = (None, 20), width = 8, relief = "sunken")
+    is_number.place(x = 475, y = 294)
     
     alert_config_text = Label(settings, text = "Alert me by...", bg = "azure", fg = "#000F46",
                 font = (None, 30, "italic")).place(x = 150, y = 365)
@@ -78,9 +79,9 @@ def settings_screen(root, coin, price, alert_type, alert_number, is_sound, is_em
                 font = (None, 22)).place(x = 250, y = 425)
     email_alert_text = Label(settings, text = "Sending me an email!", bg = "azure", fg = "#000F46",
                 font = (None, 22)).place(x = 250, y = 490)
-    email = Entry(settings, font = (None, 20), relief = "sunken")
-    email.place(x = 250, y = 530)
-    email.insert(0, "example@website.org")
+    email_address = Entry(settings, font = (None, 20), relief = "sunken")
+    email_address.place(x = 250, y = 530)
+    email_address.insert(0, "")
 
 
     #toggles
@@ -108,17 +109,21 @@ def settings_screen(root, coin, price, alert_type, alert_number, is_sound, is_em
     def changes_by_toggle(event):
         if price_changes_by["background"] == "light grey":
             price_changes_by.config(bg = "#000F46")
+            alert = "Percent"
             price_is.config(bg = "light grey")
         else:
+            alert = "None"
             price_changes_by.config(bg = "light grey")
     price_changes_by.bind("<Button-1>", changes_by_toggle)
 
     def is_toggle(event):
         if price_is["background"] == "light grey":
             price_is.config(bg = "#000F46")
+            alert = "Flat"
             price_changes_by.config(bg = "light grey")
         else:
             price_is.config(bg = "light grey")
+            alert = "None"
     price_is.bind("<Button-1>", is_toggle)
 
     def sound_alert_toggle(event):
@@ -137,34 +142,72 @@ def settings_screen(root, coin, price, alert_type, alert_number, is_sound, is_em
 
     #confirm button
     confirm = Button(settings, bg = "#0042FF", activebackground = 'dodgerblue2', fg = "antiquewhite1", activeforeground = "antiquewhite1",
-                text = "Confirm", relief = "raised", width = 10, font = (None, 30, "bold"), command = lambda: settings_confirm(root, settings))
+                text = "Confirm", relief = "raised", width = 10, font = (None, 30, "bold"), 
+                command = lambda: settings_confirm())
     confirm.place(x = 500, y = 650, anchor = "center")
+
+    #Settings confirm function
+    def settings_confirm():
+        if cryptos.get() != "" and cryptos.get() != "Update to load list of coins!":
+            coin = cryptos.get()
+        else:
+            print("Invalid coin") #TO-DO: CALL TO ERROR SCREEN HERE
+            return
+
+        if price_changes_by["background"] == "#000F46":
+            alert_type = "Percent"
+            if int(changes_number.get()) > 0 and int(changes_number.get()) < 1000:
+                alert_number = int(changes_number.get())
+            else:
+                print("Invalid changes number") #TO-DO: CALL TO ERROR SCREEN HERE
+
+        elif price_is["background"] == "#000F46" and is_choice_dropdown != "":
+            alert_type = "Flat", is_choice_dropdown.get()
+            try:
+                alert_number = float(is_number.get())
+            except ValueError: #TO-DO: CALL TO ERROR SCREEN HERE
+                print("Invalid Is number")
+        else:
+            print("Invalid alert") #TO-DO: CALL TO ERROR SCREEN HERE
+            print(lambda: alert_type)
+            return
+
+        if play_sound["background"] == "#000F46":
+            is_sound = True
+        else:
+            is_sound = False
+        
+        if send_email["background"] == "#000F46":
+            is_email = True
+            email = email_address.get()
+        else:
+            is_email = False
+        
+        print("Success") #TO-DO: REMOVE LATER
+        return coin, alert_type, alert_number, is_sound, is_email
+
+        root.deiconify()
+        settings.destroy()
+
 
     #cancel button
     cancel = Button(settings, bg = "#0042FF", activebackground = 'dodgerblue2', fg = "antiquewhite1", activeforeground = "antiquewhite1",
-                text = "Cancel", relief = "raised", width = 10, font = (None, 30, "bold"), command = lambda: settings_cancel(root, settings))
+                text = "Cancel", relief = "raised", width = 10, font = (None, 30, "bold"), command = lambda: settings_cancel())
     cancel.place(x = 200, y = 650, anchor = "center")
 
+    #settings cancel button function
+    def settings_cancel():
+        root.deiconify()
+        settings.destroy()
 
-
-#Settings confirm button
-def settings_confirm(root, settings):
-
-    root.deiconify()
-    settings.destroy()
-    return
-
-#settings cancel button
-def settings_cancel(root, settings):
-
-    root.deiconify()
-    settings.destroy()
-    return
-
-#History screen display
-def history_screen(root):
-
-    return
+#History screen display - TEST BUTTON FOR NOW
+def history_screen(root, coin, price, alert_type, alert_number, is_sound, is_email, email):
+    print(coin)
+    print(alert_type)
+    print(alert_number)
+    print(is_sound)
+    print(is_email)
+    print(email)
 
 #History back button
 def history_back():
