5aa00664b3ec0a03e8df6a53703bf874f90b795d
Haiden <hgembinski@gmail.com>
2021-07-16 22:46:51 -0400

    get price

--- ca_scraping.py
+++ ca_scraping.py
@@ -2,3 +2,17 @@
 #Scraping functionality for CryptoAlert
 
 from bs4 import BeautifulSoup
+import requests
+
+#get the price of the coin found at the url
+def get_price():
+    url = "https://coinmarketcap.com/currencies/binance-coin/"
+    price = 0
+    coinmarketcap = requests.get(url)
+    soup = BeautifulSoup(coinmarketcap.content, 'html.parser')
+    line = soup.find('div', {'priceValue___11gHJ'}).text
+
+    price = float(line[1:])
+    print(price)
+
+get_price()
