7b55b726926cb5116967d8bfc5120f2eb1196e70
Haiden <hgembinski@gmail.com>
2021-08-27 10:32:34 -0400

    Connection check implementation in update

--- crypto_alert.py
+++ crypto_alert.py
@@ -18,20 +18,31 @@ def crypto_alert():
     settings_file = "settings.txt"
     settings = ca_settings(settings_file)
 
-    if settings.get_coin() != "":
-        current_price = get_price(settings.get_url())
-        last_price = current_price
-    else:
-        current_price = ""
-        last_price = ""
+    #GUI init
+    root = tkinter.Tk()
+    gui = ca_gui(root, settings)
+
+    current_price = ""
+    last_price = ""
     initial = ""
     counter = 0
     counter2 = 0
     start = ""
+    
+    #check for internet connection
+    test_url = "http://www.google.com"
+    test_timeout = 5
 
-    #GUI init
-    root = tkinter.Tk()
-    gui = ca_gui(root, settings)
+    try:
+        print("ping")
+        test_request = requests.get(test_url, timeout = test_timeout)
+
+        if settings.get_coin() != "":
+            current_price = get_price(settings.get_url())
+            last_price = current_price
+    except:
+        print("No connection")
+        gui.set_crypto('No internet connection!')
 
 
     def update():
@@ -39,55 +50,73 @@ def crypto_alert():
         counter = counter + 1
         #print(counter)
         #print("-----")
-        if start == "":
-            start = time.time()
 
-        if settings.get_coin() != "" :
-            if settings.get_is_new():
-                current_price = get_price(settings.get_url())
-                set_ticker(root, settings, gui, current_price)
-                set_alert_info(settings, gui)
-                initial = current_price
-                settings.set_is_new(False)
-                counter = 61
-            
-            else:
-                if counter > 60:
-                    counter = 0
+        #check for internet connection
+        connected = False
+        test_url = "http://www.google.com"
+        test_timeout = 5
 
-                    if current_price != "" and last_price != current_price:
-                        last_price = current_price
+        try:
+            print("ping")
+            test_request = requests.get(test_url, timeout = test_timeout)
+            connected = True
+        except:
+            print("No connection")
+            gui.set_crypto('No internet connection!')
 
+        if connected:
+            print("ping success")
+
+            if start == "":
+                start = time.time()
+
+            if settings.get_coin() != "" :
+                if settings.get_is_new():
                     current_price = get_price(settings.get_url())
+                    set_ticker(root, settings, gui, current_price)
+                    set_alert_info(settings, gui)
+                    initial = current_price
+                    settings.set_is_new(False)
+                    counter = 61
+                
+                else:
+                    gui.set_crypto(settings.get_symbol() + "-" + settings.get_coin())
+                    if counter > 60:
+                        counter = 0
 
-                    if alert_check(settings, initial, current_price):
-                        print("Alert!")
-                        to_history('history.txt', '{month}/{day}/{year},{time},{symbol},${price},{email_sent}'.format(month = date.today().month, day = date.today().day,
-                            year = date.today().year, time = strftime("%H:%M:%S", localtime()), symbol = settings.get_symbol(), 
-                            price = current_price.replace(",",""), email_sent = settings.get_email_status()))
-                        gui.show_alert_screen(root, settings, initial)
-                        
-                        settings.blank_settings()
-                        delete_file(settings_file)
-                        gui.default_display()
+                        if current_price != "" and last_price != current_price:
+                            last_price = current_price
 
-                    elif current_price != "":
-                        if float(current_price.replace(",","")) > float(last_price.replace(",","")):
-                            gui.set_price_color("lime green")
-                        elif float(current_price.replace(",","")) < float(last_price.replace(",","")):
-                            gui.set_price_color("red")
-                        
-                        gui.set_price("$" + current_price)
-                        print("------------------------")
-                        print(counter2)
-                        print("Initial Price: " + initial)
-                        print("Current Price: " + current_price)
-                        print("Last Price: " + last_price)
-                        counter2 = counter2 + 1
-                        end = time.time()
-                        print("Elapsed time: " + str(end - start))
-                        start = time.time()
-                        end = 0
+                        current_price = get_price(settings.get_url())
+
+                        if alert_check(settings, initial, current_price):
+                            print("Alert!")
+                            to_history('history.txt', '{month}/{day}/{year},{time},{symbol},${price},{email_sent}'.format(month = date.today().month, day = date.today().day,
+                                year = date.today().year, time = strftime("%H:%M:%S", localtime()), symbol = settings.get_symbol(), 
+                                price = current_price.replace(",",""), email_sent = settings.get_email_status()))
+                            gui.show_alert_screen(root, settings, initial)
+                            
+                            settings.blank_settings()
+                            delete_file(settings_file)
+                            gui.default_display()
+
+                        elif current_price != "":
+                            if float(current_price.replace(",","")) > float(last_price.replace(",","")):
+                                gui.set_price_color("lime green")
+                            elif float(current_price.replace(",","")) < float(last_price.replace(",","")):
+                                gui.set_price_color("red")
+                            
+                            gui.set_price("$" + current_price)
+                            print("------------------------")
+                            print(counter2)
+                            print("Initial Price: " + initial)
+                            print("Current Price: " + current_price)
+                            print("Last Price: " + last_price)
+                            counter2 = counter2 + 1
+                            end = time.time()
+                            print("Elapsed time: " + str(end - start))
+                            start = time.time()
+                            end = 0
         root.after(500, update)
 
 
